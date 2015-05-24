a0c2002124d0cc61a65d21542962f7f557f32b8d
Will Chiong <will@infomofo.com>
2015-05-24 14:49:59 -0400

    add fab shuffle

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,16 @@
  * Controller of the infomofogithubioApp
  */
 angular.module('infomofogithubioApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $timeout) {
+
+    $scope.dataModel = {
+      shuffleIcon: 'shuffle'
+    };
+
+    $scope.shuffle = function() {
+      $scope.dataModel.shuffleIcon = 'autorenew';
+      $timeout(function() {
+        $scope.dataModel.shuffleIcon = 'shuffle';
+      }, 750);
+    };
   });
