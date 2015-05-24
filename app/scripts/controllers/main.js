d82f67d299ab614bdd8fbd35694617818fc4bbde
Will Chiong <will@infomofo.com>
2015-05-24 15:41:00 -0400

    finish shuffling outfit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -32,10 +32,33 @@ angular.module('infomofogithubioApp')
       'images/shirt10.jpg'
     ];
 
+    $scope.pants = [
+      'images/pants1.jpg',
+      'images/pants2.jpg',
+      'images/pants3.jpg',
+      'images/pants4.jpg',
+      'images/pants5.jpg',
+      'images/pants6.jpg',
+      'images/pants7.jpg'
+    ];
+
+    $scope.shoes = [
+      'images/shoes1.jpg',
+      'images/shoes2.jpg',
+      'images/shoes3.jpg',
+      'images/shoes4.jpg',
+      'images/shoes5.jpg',
+      'images/shoes6.jpg',
+      'images/shoes7.jpg',
+      'images/shoes8.jpg'
+    ];
+
     $scope.dataModel = {
       shuffleIcon: 'shuffle',
       selectedShirt: 0,
-      selectedHead: 0
+      selectedHead: 0,
+      selectedPants: 0,
+      selectedShoes: 0
     };
 
     var randomIndex = function(length) {
@@ -48,10 +71,24 @@ angular.module('infomofogithubioApp')
       $scope.dataModel.selectedShirt = index;
     };
 
+    $scope.selectHead = function(index) {
+      $scope.dataModel.selectedHead = index;
+    };
+
+    $scope.selectPants = function(index) {
+      $scope.dataModel.selectedPants = index;
+    };
+
+    $scope.selectShoes = function(index) {
+      $scope.dataModel.selectedShoes = index;
+    };
+
     $scope.shuffle = function() {
       $scope.dataModel.shuffleIcon = 'autorenew';
       $scope.dataModel.selectedShirt = randomIndex($scope.shirts.length);
       $scope.dataModel.selectedHead = randomIndex($scope.heads.length);
+      $scope.dataModel.selectedPants = randomIndex($scope.pants.length);
+      $scope.dataModel.selectedShoes = randomIndex($scope.shoes.length);
       $timeout(function() {
         $scope.dataModel.shuffleIcon = 'shuffle';
       }, 250);
